da18b78099a014574d54bd2937e6c2a325d23e88
Shivoy Arora <85881270+arorashivoy@users.noreply.github.com>
2023-04-03 18:44:56 +0530

    Initial Commit

---
+++ ARDice/ARDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARDiceApp.swift
+//  ARDice
+//
+//  Created by Shivoy Arora on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ARDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
